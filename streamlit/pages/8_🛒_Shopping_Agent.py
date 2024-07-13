0a671b7dbb91d518090c2c5b3e62ae18079be535
Abbas <fibeg@amazon.com>
2024-07-13 21:08:07 +0200

    added address form and buy function

--- streamlit/pages/8_🛒_Shopping_Agent.py
+++ streamlit/pages/8_🛒_Shopping_Agent.py
@@ -27,6 +27,7 @@ agent_options = [
 st.set_page_config(
     page_title=f"AnyCompanyCommerce Shopping Agent",
     page_icon="🛍️",
+    layout='wide'
 )
 
 
@@ -47,6 +48,9 @@ def init_state():
     st.session_state.agent_assistant = getAgentAssistant()
     st.session_state.messages.append({"role": "assistant", "content": welcome_message})
     st.session_state.selected_product = None
+    st.session_state.buy_product = None
+    st.session_state.user_action = None
+    st.session_state.shipping_details_provided = None
     st.session_state.answer = None
 
     # Get the current time in the user's timezone
@@ -126,18 +130,105 @@ def show_product(product):
     user_query = f'View details for **{product['product_name']}**'
     st.session_state.messages.append({"role": "user", "content": user_query})
 
+def buy_product(product):
+    print('test buy click')
+    st.session_state.buy_product = product
+    st.session_state.user_action = 'BUY_PRODUCT'
+    user_query = f"Buy product : **{ product['name']}**"
+    st.session_state.messages.append({"role": "user", "content": user_query})
+
+def add_product(product):
+    print('test add click')
+    st.session_state.buy_product = product
+    st.session_state.user_action = 'ADD_PRODUCT'
+    user_query = f"Add product { product['name']} to order"
+    st.session_state.messages.append({"role": "user", "content": user_query})
+
+# The JSON object representing the shipping address structure
+shipping_address_structure = {
+    "email": "",
+    "firstName": "",
+    "lastName": "",
+    "address": "",
+    "city": "",
+    "zipCode": "",
+    "state": "",
+    "country": ""
+}
+
+def submit_callback():
+    form_data = {
+        "Email": st.session_state.email,
+        "First Name": st.session_state.first_name,
+        "Last Name": st.session_state.last_name,
+        "Address": st.session_state.address,
+        "City": st.session_state.city,
+        "Zip Code": st.session_state.zip_code,
+        "State": st.session_state.state,
+        "Country": st.session_state.country
+    }
+
+    # Format the form data as a string with key-value pairs, using HTML <br> tags for line breaks
+    formatted_data = "<br>".join(f"{key}: {value}" for key, value in form_data.items())
+    
+    formatted_details = f"""
+        Shipping address details: <br>
+        {formatted_data}
+        """
+
+    st.session_state.messages.append({"role": "user", "content": formatted_details})
+    st.session_state.shipping_details_provided = form_data
+
+def create_shipping_form():
+    with st.form("shipping_address_form"):
+        st.write("## Shipping Address")
+        
+        # Email in a single column
+        st.text_input("Email", key="email", value=st.session_state.get('email', 'john.doe@xyz.com'))
+        
+        # Create two columns for the form
+        col1, col2 = st.columns(2)
+        
+        # First Name and Last Name in the same row
+        with col1:
+            st.text_input("First Name", key="first_name", value=st.session_state.get('first_name', 'John'))
+        with col2:
+            st.text_input("Last Name", key="last_name", value=st.session_state.get('last_name', 'Doe'))
+        
+        # Address in a single column
+        st.text_input("Address", key="address", value=st.session_state.get('address', 'ABC X street'))
+        
+        # City and Zip Code in the same row
+        with col1:
+            st.text_input("City", key="city", value=st.session_state.get('city', 'Stockholm'))
+        with col2:
+            st.text_input("Zip Code", key="zip_code", value=st.session_state.get('zip_code', '336647'))
+        
+        # State and Country in the same row
+        with col1:
+            st.text_input("State", key="state", value=st.session_state.get('state', 'Stockholm'))
+        with col2:
+            st.text_input("Country", key="country", value=st.session_state.get('country', 'Sweden'))
+        
+    
+        # Center the submit button and give it a custom width
+        col1, col2, col3 = st.columns([1,2,1])
+        with col2:
+            st.form_submit_button("Submit", on_click=submit_callback,  use_container_width=True)
+    
+
 def load_demo():
 
-    chat_container = st.container(height=350)
+    chat_container = st.container(height=450)
     
     for message in st.session_state.messages:
         # with st.chat_message(message["role"]):
         chat_container.chat_message(message["role"]).markdown(message["content"], unsafe_allow_html=True)
     
-
     user_query = st.chat_input(placeholder="Ask me anything!")
     if user_query:
         st.session_state.selected_product = None
+        st.session_state.buy_product = None
         st.session_state.messages.append({"role": "user", "content": user_query})
 
         # with st.chat_message("user"):
@@ -160,14 +251,15 @@ def load_demo():
                     products_history= f""" """
                     for i, product in enumerate(products, 1):
                         products_history += f"""
-                        | <img src="{product["image_url"]}" width="100" alt="{product["product_name"]}"> | {i}. **{product["product_name"]}** |
+                        | <img src="{product["image_url"]}" width="100" alt="{product["product_name"]}"> | {i}. **{product["product_name"]}** | Price: ${product["price"]} |
                         """
 
                         col1, col2 = st.columns([1, 2])
                         with col1:
-                            st.image(product['image_url'], width=100)
+                            st.image(product['image_url'])
                         with col2:
                             st.write(f"{i}. {product['product_name']}")
+                            st.write(f"${product['price']}")
                             st.button(f"View Details", key=f"show_{product['product_id']}", on_click=show_product, args=(product,))
 
                 #st.markdown( response["output_text"])
@@ -178,29 +270,69 @@ def load_demo():
                     st.session_state.messages.append({"role": "assistant", "content": products_history})
                 st.session_state.trace = response["trace"]
 
+    if st.session_state.buy_product:
+        product_to_buy = st.session_state.buy_product
+        if st.session_state.user_action == 'BUY_PRODUCT':
+            query = f"I want to buy product { product_to_buy['name']}"
+        else:
+            query = f"I want to add product { product_to_buy['name']} to the order"
+        
+        if st.session_state.shipping_details_provided is not None:
+            data = st.session_state.shipping_details_provided
+            query+= f"""
+
+                <shipping_address>
+                {json.dumps(data, indent=2)}
+                </shipping_address>
+            """
+
+        with chat_container.chat_message("assistant"):
+            # Add a spinner to show loading state
+            with st.spinner('...'):
+                if st.session_state.shipping_details_provided is None:
+                    st.markdown("Please provide your shipping address details:")
+                    # Use a container with custom width for the form
+                    with st.container():
+                        _, col, _ = st.columns([1, 2, 1])  # This creates a centered column
+                        with col:
+                            create_shipping_form()
+                else:
+                    st.session_state.buy_product = None
+                    response = GetAnswers(f"{query} with id {product_to_buy['id']}", st.session_state.session_id, st.session_state.agent_assistant, agent_option["value"], st.session_state.agent_session_state)
+                    print(response["output_text"])
+                    st.markdown(response["output_text"])
+                    st.session_state.messages.append({"role": "assistant", "content": response["output_text"]})
+
     # Display selected product details
     if st.session_state.selected_product:
         product_id = st.session_state.selected_product['product_id']
-        with st.spinner('...'):
-            product = get_product_details(product_id)
+        with chat_container.chat_message("assistant"):  
+            with st.spinner('...'):
+                st.session_state.selected_product = None
+                product = get_product_details(product_id)
 
-            print(product)
+                print(product)
 
-            if product: 
-                with chat_container.chat_message("assistant"):         
+                if product: 
                     col1, col2 = st.columns([2, 1])
                     with col1:
-                        st.image(product['image'], width=300)
+                        st.image(product['image'], width=500)
                         st.write(f"<small>{product['description']}</small>", unsafe_allow_html=True)
                     with col2:
                         st.write(f"#### {product['name']}")
                         st.write(f"<small>Category: {product['category'].capitalize()} | Style: {product['style'].capitalize()}</small>", unsafe_allow_html=True)
                         st.write(f"${product['price']:.2f}")
-                        if product['promoted'] == "True":
+                        if 'promoted' in product and product['promoted'] == "True":
                             st.write("🔥 **Promoted Item**")
                         if product['current_stock'] > 0:
-                            if st.button("Buy Now", key=f"buy_{product['id']}"):
-                                st.success(f"{product['name']} added to cart!")
+                            col1, col2 = st.columns(2)
+                            with col1:
+                                 st.button("Buy Now", key=f"buy_{product['id']}", on_click=buy_product, args=(product,))
+                            # with col2:
+                            #      st.button("Add to Order", key=f"add_{product['id']}", on_click=add_product, args=(product,))
+                           
+                            # if st.button("Buy Now", key=f"buy_{product['id']}"):
+                            #     st.success(f"{product['name']} added to cart!")
                         else:
                             st.write("**Out of Stock**")
                         st.write(f"<small>Current Stock: {product['current_stock']} units</small>", unsafe_allow_html=True)
@@ -216,7 +348,7 @@ def load_demo():
                     #### {product['name']}
 
                     Category: {product['category'].capitalize()} | Style: {product['style'].capitalize()}  
-                    {'🔥 **Promoted Item**' if product['promoted'] == "True" else ''}
+                    {'🔥 **Promoted Item**' if 'promoted' in product and product['promoted'] == "True" else ''}
 
                     Price: ${product['price']:.2f}
 
@@ -229,9 +361,9 @@ def load_demo():
                         selected_content += f"\n\n**Also known as:** {', '.join(product['aliases'])}"
                     
                     st.session_state.messages.append({"role": "assistant", "content": selected_content})
-            else:
-                error = 'Apologies, there seems to be temporary issues in getting product details at the moment. Please try again later.'
-                st.session_state.messages.append({"role": "assistant", "content": error})
+                else:
+                    error = 'Apologies, there seems to be temporary issues in getting product details at the moment. Please try again later.'
+                    st.session_state.messages.append({"role": "assistant", "content": error})
             
 
 
@@ -280,6 +412,11 @@ def load_trace():
         else:
             st.text("None")
 
+def load_session():
+    st.write(f"Session ID: {st.session_state.session_id}")
+    if st.session_state.agent_session_state:
+        st.json(st.session_state.agent_session_state)
+
 def main():
     # Initialize the messages and assistant object using session state
     if "messages" not in st.session_state or "agent_assistant" not in st.session_state:
@@ -288,20 +425,23 @@ def main():
     # with st.expander("Architecture"):
     #     load_arch()
         
-    col1, col2,  = st.columns([2,1])
-    with col1:
-        st.write(f"Session ID: {st.session_state.session_id}")
+    # col1, col2,  = st.columns([2,1])
+    # with col1:
+    #     st.write(f"Session ID: {st.session_state.session_id}")
 
-    with col2:
-        if st.button("Clear message history"):
-            init_state()
+    # with col2:
+    #     if st.button("Clear message history"):
+    #         init_state()
 
-    chat_demo, trace,  = st.tabs(["Assistant", "Trace"])
+    chat_demo, session, trace,  = st.tabs(["Assistant", "Session", "Trace"])
+    with session:
+        load_session()
     with chat_demo:
         load_demo()
     with trace:
         load_trace()
-    
+  
+
     users_data = fetch_random_users(10)
 
     # Create a list of user options
@@ -318,8 +458,6 @@ def main():
     if st.session_state.email_confirmation:
         st.write(st.session_state.email_confirmation)
     
-    if st.session_state.agent_session_state:
-        st.json(st.session_state.agent_session_state)
 
 agent_option = st.sidebar.selectbox("Select an agent", agent_options, format_func=lambda option: option["label"])
 if agent_option['label'] == "Support Agent":
@@ -344,7 +482,14 @@ if __name__ == "__main__":
 
     keywords = [f'Amazon Bedrock Agent: Anthropic Claude Sonnet', 'OpenSearch Serverless']
     formatted_labels = [keyword_label(keyword) for keyword in keywords]
-    st.write(' '.join(formatted_labels), unsafe_allow_html=True)
+    col1, col2,  = st.columns([2,1])
+    with col1:
+        st.write(' '.join(formatted_labels), unsafe_allow_html=True)
+
+    with col2:
+        if st.button("Clear message history"):
+            init_state()
+            
     apply_studio_style()
 
     
